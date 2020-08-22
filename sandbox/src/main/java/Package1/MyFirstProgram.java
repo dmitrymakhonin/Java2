c42172d2a41daa4ac23036cd9db7259bec96026a
dmitrymakhonin <46838135+dmitrymakhonin@users.noreply.github.com>
2020-08-22 14:26:08 +0300

    settings

--- sandbox/src/main/java/Package1/MyFirstProgram.java
+++ sandbox/src/main/java/Package1/MyFirstProgram.java
@@ -1,8 +1,8 @@
 package Package1;
 
 public class MyFirstProgram {
-public static void main(String[] args){
+  public static void main(String[] args) {
 
-System.out.println("Hello, world!");
-}
+    System.out.println("Hello, world!");
+  }
 }
